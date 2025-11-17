dee09b499cefd81600da59e5cae1b7bb4e5e8ee4
AzarconAD <200471951+AzarconAD@users.noreply.github.com>
2025-11-17 16:02:15 +0800

    Update queue_card_game.py
    added stack data structure

--- queue_card_game.py
+++ queue_card_game.py
@@ -1,73 +1,99 @@
 import random
 
-def shuffle_cards():
-    cards = {"hearts":{
-                    "ace":1,
-                    "2":2, 
-                    "3":3, 
-                    "4":4, 
-                    "5":5, 
-                    "6":6, 
-                    "7":7, 
-                    "8":8, 
-                    "9":9, 
-                    "10":10, 
-                    "jack": 11, 
-                    "queen": 12, 
-                    "king":13
-                    },
-            "diamonds":{
-                    "ace":1,
-                    "2":2, 
-                    "3":3, 
-                    "4":4, 
-                    "5":5, 
-                    "6":6, 
-                    "7":7, 
-                    "8":8, 
-                    "9":9, 
-                    "10":10, 
-                    "jack": 11, 
-                    "queen": 12, 
-                    "king":13
-                    },
-            "clubs":{
-                    "ace":1,
-                    "2":2, 
-                    "3":3, 
-                    "4":4, 
-                    "5":5, 
-                    "6":6, 
-                    "7":7, 
-                    "8":8, 
-                    "9":9, 
-                    "10":10, 
-                    "jack": 11, 
-                    "queen": 12, 
-                    "king":13
-                    },
-            "spades":{
-                    "ace":1,
-                    "2":2, 
-                    "3":3, 
-                    "4":4, 
-                    "5":5, 
-                    "6":6, 
-                    "7":7, 
-                    "8":8, 
-                    "9":9, 
-                    "10":10, 
-                    "jack": 11, 
-                    "queen": 12, 
-                    "king":13
+class deckOfCards:
+    def shuffle_cards(self):
+        cards = {"hearts":{
+                        "ace":1,
+                        "2":2, 
+                        "3":3, 
+                        "4":4, 
+                        "5":5, 
+                        "6":6, 
+                        "7":7, 
+                        "8":8, 
+                        "9":9, 
+                        "10":10, 
+                        "jack": 11, 
+                        "queen": 12, 
+                        "king":13
+                        },
+                "diamonds":{
+                        "ace":1,
+                        "2":2, 
+                        "3":3, 
+                        "4":4, 
+                        "5":5, 
+                        "6":6, 
+                        "7":7, 
+                        "8":8, 
+                        "9":9, 
+                        "10":10, 
+                        "jack": 11, 
+                        "queen": 12, 
+                        "king":13
+                        },
+                "clubs":{
+                        "ace":1,
+                        "2":2, 
+                        "3":3, 
+                        "4":4, 
+                        "5":5, 
+                        "6":6, 
+                        "7":7, 
+                        "8":8, 
+                        "9":9, 
+                        "10":10, 
+                        "jack": 11, 
+                        "queen": 12, 
+                        "king":13
+                        },
+                "spades":{
+                        "ace":1,
+                        "2":2, 
+                        "3":3, 
+                        "4":4, 
+                        "5":5, 
+                        "6":6, 
+                        "7":7, 
+                        "8":8, 
+                        "9":9, 
+                        "10":10, 
+                        "jack": 11, 
+                        "queen": 12, 
+                        "king":13
+                        }
                     }
-                }
-    
-    deck = []
-    for suit in cards:
-        for card_name in cards[suit]:
-            deck.append((suit, card_name, cards[suit][card_name]))
-    
-    random.shuffle(deck)
-    
-    return deck
+        
+        deck = []
+        for suit in cards:
+            for card_name in cards[suit]:
+                deck.append((suit, card_name, cards[suit][card_name]))
+        
+        random.shuffle(deck)
+        
+        return deck
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, data):
+        new_node = Node(data)
+        if self.top:
+            new_node.next = self.top
+        self.top = new_node
+
+
+    def pop(self):
+        if self.top is None:
+            return None
+        else:
+            popped_node = self.top
+            self.top = self.top.next
+            popped_node.next = None
+            return popped_node.data
