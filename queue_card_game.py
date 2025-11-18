05bd65705b079cb640302fb3038fe3fdad2aadda
AzarconAD <200471951+AzarconAD@users.noreply.github.com>
2025-11-18 09:48:55 +0800

    Update queue_card_game.py
    updated self.cards format
    added game functions

--- queue_card_game.py
+++ queue_card_game.py
@@ -2,75 +2,17 @@ import random
 
 class deckOfCards:
     def __init__(self):
-        self.cards = {"hearts":{
-                        "ace":1,
-                        "2":2, 
-                        "3":3, 
-                        "4":4, 
-                        "5":5, 
-                        "6":6, 
-                        "7":7, 
-                        "8":8, 
-                        "9":9, 
-                        "10":10, 
-                        "jack": 11, 
-                        "queen": 12, 
-                        "king":13
-                        },
-                "diamonds":{
-                        "ace":1,
-                        "2":2, 
-                        "3":3, 
-                        "4":4, 
-                        "5":5, 
-                        "6":6, 
-                        "7":7, 
-                        "8":8, 
-                        "9":9, 
-                        "10":10, 
-                        "jack": 11, 
-                        "queen": 12, 
-                        "king":13
-                        },
-                "clubs":{
-                        "ace":1,
-                        "2":2, 
-                        "3":3, 
-                        "4":4, 
-                        "5":5, 
-                        "6":6, 
-                        "7":7, 
-                        "8":8, 
-                        "9":9, 
-                        "10":10, 
-                        "jack": 11, 
-                        "queen": 12, 
-                        "king":13
-                        },
-                "spades":{
-                        "ace":1,
-                        "2":2, 
-                        "3":3, 
-                        "4":4, 
-                        "5":5, 
-                        "6":6, 
-                        "7":7, 
-                        "8":8, 
-                        "9":9, 
-                        "10":10, 
-                        "jack": 11, 
-                        "queen": 12, 
-                        "king":13
-                        }
-                    }
+        self.suits = ["hearts", "diamonds", "clubs", "spades"]
+        self.card_names = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+        self.values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    
     def shuffle_cards(self):
-        deck = []
-        for suit in self.cards:
-            for card_name in self.cards[suit]:
-                deck.append((suit, card_name, self.cards[suit][card_name]))
+        deck = [(suit, card_name, value) 
+                for suit in self.suits 
+                for card_name, value in zip(self.card_names, self.values)]
         
         random.shuffle(deck)
-        
+        return deck
         return deck
 
 class Queue:
@@ -85,17 +27,55 @@ class Queue:
     
     def display(self):
         return self.queue
-    
-#-----actual game------#
 
+class Banker(Queue):
+    def hold(self):
+        return self.display()
+    
+    def draw(self):
+        if self.queue:
+            drawn_card = self.dequeue()
+            return drawn_card
+        else:
+            return None
+        
+    def has_cards(self):
+        return len(self.queue) > 0
+
+#-----initialize game-----#
 deck = deckOfCards()
 shuffled_deck = deck.shuffle_cards()
-queue = Queue()
 
+banker = Banker()
+
+#-----enqueueing 5 cards to the banker's queue (hold)-----#
 for card in shuffled_deck[1:6]:
-    queue.enqueue(card)
+    banker.enqueue(card)
 
-player = deck.shuffle_cards()[0]
+player_card = shuffled_deck[0]
 
-print("banker card:", queue.display())
-print("Player card:", player)
+#-----actual game-----#
+print("Player's card:", player_card)
+
+round_number = 1
+while banker.has_cards():
+    drawn_card = banker.draw()
+    print(f"\nRound {round_number}: Banker draws {drawn_card}")
+    
+    player_value = player_card[2]
+    banker_value = drawn_card[2]
+    
+    if player_value > banker_value:
+        print("Player wins this round!")
+    elif player_value < banker_value:
+        print("Banker wins this round!")
+    else:
+        print("It's a tie this round!")
+
+    choice = input("Would you like to keep playing? (y/n): ")
+    if choice == "y":
+        round_number += 1
+    elif choice == "n":
+        break
+
+print("\nGame over!")
