a213e226af538023d8c7d83ab1f8800f6f54f2ac
AzarconAD <200471951+AzarconAD@users.noreply.github.com>
2025-11-20 22:34:07 +0800

    Create binary_tree_plinko_game.py
    initialized binary tree data structure

---
+++ binary_tree_plinko_game.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self, root_value=None):
+        self.root = Node(root_value) if root_value is not None else None
+
+    def insert_left(self, current_node, value):
+        if current_node.left is None:
+            current_node.left = Node(value)
+        else:
+            new_node = Node(value)
+            new_node.left = current_node.left
+            current_node.left = new_node
+
+    def insert_right(self, current_node, value):
+        if current_node.right is None:
+            current_node.right = Node(value)
+        else:
+            new_node = Node(value)
+            new_node.right = current_node.right
+            current_node.right = new_node
